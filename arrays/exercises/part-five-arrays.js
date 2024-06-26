f616c92f1298940574292d3fbc4e6e680f77c932
Kai P <pouxkai@gmail.com>
2024-06-26 13:44:21 -0500

    arrays exercise

--- arrays/exercises/part-five-arrays.js
+++ arrays/exercises/part-five-arrays.js
@@ -2,10 +2,11 @@ let str = 'In space, no one can hear you code.';
 let arr = ['B', 'n', 'n', 5];
 
 //1) Use the split method on the string to identify the purpose of the parameter inside the ().
-
+console.log(str.split()); console.log(str.split('e'));//removes what's inside the parenthesis
 //2) Use the join method on the array to identify the purpose of the parameter inside the ().
-
+console.log(arr.join()); console.log(arr.join('a')); console.log(arr.join(' '));console.log(arr.join(''));// inside the paranthesis goes between each element, commas are removed
 //3) Do split or join change the original string/array?
-
+//Only when there's a parameter inside the parenthesis
 //4) We can take a comma-separated string and convert it into a modifiable array. Try it! Alphabetize the cargoHold string, and then combine the contents into a new string.
 let cargoHold = "water,space suits,food,plasma sword,batteries";
+cargoHold = cargoHold.split(','); console.log(cargoHold.sort()); console.log(cargoHold.join(','));
